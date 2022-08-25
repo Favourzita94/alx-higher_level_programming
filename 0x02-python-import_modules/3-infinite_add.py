e0a428e0a379ce4e46c3b5c251a6ec5b5431615e
Favourzita94 <amarachinkemdilim@yahoo.com>
2022-08-25 06:42:35 -0700

    Commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
-    sum = 0
-    for i in range(1, len(sys.argv)):
-        sum += int(sys.argv[i])
-    print("{}".format(sum)
+
+    n = len(sys.argv)
+    res = 0
+
+    for i in range(1, n):
+        res = res + int(sys.argv[i])
+    print(f"{res:d}")
